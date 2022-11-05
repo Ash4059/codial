a4c7cebf3a4ef1a7249d08dc912e05390c856c7d
zeuslearning-ashish-jaiswal <ashish.jaiswal@zeuslearning.com>
2022-11-05 13:32:55 +0530

    Showing all post completed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,10 @@
+const Posts = require('../models/posts');
+
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title: "Home"
-    });
+    Posts.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
 }
